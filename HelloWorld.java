d00cf8ff6c69cde5f16fce971158aa41974ad68a
tdattolo <tdattolo@iu.edu>
2020-01-15 23:53:02 -0500

    bed

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * HelloWorld
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("H" + "\n" + "e" + "\n" + "l" + "\n" + 
+        "l" + "\n" + "o");
+    }
+}

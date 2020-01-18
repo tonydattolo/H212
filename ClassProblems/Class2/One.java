0ab5290e186dd7bf6c82dfcf9151640f042c8324
tdattolo <tdattolo@iu.edu>
2020-01-17 21:19:31 -0500

    lab2

--- ClassProblems/Class2/One.java
+++ ClassProblems/Class2/One.java
@@ -2,28 +2,21 @@ package ClassProblems.Class2;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import java.awt.*;
-/**
- * strings0 are IMMUTABLE
- */
+
 public class One extends JComponent {
-
-    public One {
-        this.p = new Penguin();
-    }
-    public static void main(String[] args) {
-        JFrame a = new JFrame("Blue pill, or red?");
-        a.setVisible(true);
-        a.setSize(500,400);
-
-        One b = new One();
-        // System.out.println(b);
-        a.add(b);
-    }
-
-    public void paintComponent(Graphics g){
-        // g.setColor(Color.YELLOW);
-        // g.fillOval(100, 100, 200, 200);
-        this.p.draw();
-    }
-
+  Penguin p; // define it 
+  public One() { // don't forget the parens 
+    this.p = new Penguin("Tony Danza"); // pass the name 
+  }
+  public static void main(String[] args) {
+    JFrame a = new JFrame("Blue pill, or red?");
+    a.setVisible(true);
+    a.setSize(500,400);
+    
+    One b = new One();
+    a.add(b);
+  }
+  public void paintComponent(Graphics g){
+    this.p.draw(g); // draw has an argument, provide
+  }  
 }
--- ClassProblems/Class2/Penguin.java
+++ ClassProblems/Class2/Penguin.java
@@ -1,32 +1,17 @@
 package ClassProblems.Class2;
 import java.awt.*;
-/**
- * penguin
- */
+
 public class Penguin {
-
-    public Penguin(String name) {
-        this.name = name;
-    }
-
-    private String name = "Tommy Gufano";
-    String nickname = "The Big Sleazy";
-    public void talk() {
-        System.out.println("Howdy" + this.name + " otherwise known as, " + this.nickname);    
-    }
-    public void draw(Graphics g){
-        // THis is Lab2
-        g.setColor(Color.YELLOW);
-        g.fillOval(100, 100, 200, 200);
-     
-    }
+  public Penguin(String name) {
+    this.name = name;
+  }
+  private String name = "Tommy Gufano";
+  String nickname = "The Big Sleazy";
+  public void talk() {
+    System.out.println("Howdy" + this.name + " otherwise known as, " + this.nickname);    
+  }
+  public void draw(Graphics g){
+    g.setColor(Color.YELLOW);
+    g.fillOval(100, 100, 200, 200);
+  }
 }
-
-
-// public class Penguin() {
-//     String name = "Tommy The Big Sleazy Gufano";
-//     public void talk() {
-//         System.out.println("Howdy" + name + " otherwise known as, " + this);    
-//     }
-// }
-

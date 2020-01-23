4c2ded21939e85cc16b4020a26c0250d766014bd
tdattolo <tdattolo@iu.edu>
2020-01-22 22:25:29 -0500

    Homework01 finished

--- Homework/Homework01.java
+++ Homework/Homework01.java
@@ -1,10 +1,36 @@
 package Homework;
-import java.util.Dictionary;
-
+import java.math.BigInteger;
+import java.util.*;
 
 public class Homework01 {
-    Dictionary d = new Dictionary();
-    public static int fib(int n) {
+
+    private static BigInteger x = new BigInteger("1");
+    public static void main(String[] args) {
+        // Scanner for terminal number input 
+        // Scanner scan = new Scanner(System.in);
+        // System.out.println("Enter # for Fibonacci Computation: ");
+        // int number = scan.nextInt();
+        // System.out.println("Fibonacci of " + number + ": " + fib(number));
+
+        // Fixed variable for debugging
+        // String number = "34";
+
+        // Command Line Argument input
+        String number = args[0];
+        int n = Integer.parseInt(number);
+        System.out.println(fib(n));
+
+    }
+    public static BigInteger fib(int index){
+        if (index == 0) return x;
+        else if (index == 1) return x;
+        return fibHelper(1,index,x,x);
         
+    }
+    public static BigInteger fibHelper(int location, int target, BigInteger a, BigInteger b){
+        if (location == target) return b;
+        else{
+            return fibHelper(location + 1, target, b, a.add(b));
+        }
     }
 }
